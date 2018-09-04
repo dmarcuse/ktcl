370ab0524a1b678269cc12a18312ae786ff2a812
apemanzilla <dominic@marcuse.us>
2018-09-03 22:24:48 -0400

    Use UInt/ULong in info wrapper

--- src/main/kotlin/me/apemanzilla/ktcl/CLInfoWrapper.kt
+++ src/main/kotlin/me/apemanzilla/ktcl/CLInfoWrapper.kt
@@ -12,9 +12,6 @@ import java.nio.ByteBuffer
 internal typealias WrappedInfoFunc = (Int, ByteBuffer?, PointerBuffer?) -> Int
 internal typealias InfoFunc = (Long, Int, ByteBuffer?, PointerBuffer?) -> Int
 
-private val Int.unsigned get() = Integer.toUnsignedLong(this)
-private val Long.unsigned get() = BigInteger(1, BigInteger.valueOf(this).toByteArray())
-
 /**
  * A convenience class to simplify accessing information for OpenCL objects
  */
@@ -57,9 +54,9 @@ class CLInfoWrapper internal constructor(val fn: WrappedInfoFunc) {
 	fun bytes(p: Int) = raw(p).toArray()
 	fun shorts(p: Int) = raw(p).asShortBuffer().toArray()
 	fun ints(p: Int) = raw(p).asIntBuffer().toArray()
-	fun uints(p: Int) = ints(p).map { it.unsigned }
+	fun uints(p: Int) = ints(p).map { it.toUInt() }
 	fun longs(p: Int) = raw(p).asLongBuffer().toArray()
-	fun ulongs(p: Int) = longs(p).map { it.unsigned }
+	fun ulongs(p: Int) = longs(p).map { it.toULong() }
 	fun floats(p: Int) = raw(p).asFloatBuffer().toArray()
 	fun doubles(p: Int) = raw(p).asDoubleBuffer().toArray()
 	fun pointers(p: Int) = raw(p).asPointerBuffer().toArray()
@@ -69,9 +66,9 @@ class CLInfoWrapper internal constructor(val fn: WrappedInfoFunc) {
 	fun byte(p: Int) = raw(p, 1)[0]
 	fun short(p: Int) = raw(p, 2).asShortBuffer()[0]
 	fun int(p: Int) = raw(p, 4).asIntBuffer()[0]
-	fun uint(p: Int) = int(p).unsigned
+	fun uint(p: Int) = int(p).toUInt()
 	fun long(p: Int) = raw(p, 8).asLongBuffer()[0]
-	fun ulong(p: Int) = long(p).unsigned
+	fun ulong(p: Int) = long(p).toULong()
 	fun float(p: Int) = raw(p, 4).asFloatBuffer()[0]
 	fun double(p: Int) = raw(p, 8).asDoubleBuffer()[0]
 	fun pointer(p: Int) = raw(p).asPointerBuffer()[0]
